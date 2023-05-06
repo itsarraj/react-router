124be79b0232a7100833bf26489f3707e914a4c2
Animesh Raj <animesh080808@gmail.com>
2023-05-06 21:49:54 +0530

    ✨ feat : useNavigate() ,  useNavigate() most of the time
    Signed-off-by: Animesh Raj <animesh080808@gmail.com>

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,7 +1,17 @@
-import { Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export function NotFoundPage() {
-    return <Navigate to="/" />;
+    // this is the way we navigate in functional components & we use useNavigate() most of the time
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        setTimeout(() => {
+            navigate('/');
+        }, 1000);
+    }, []);
+
+    return <h1>NotFoundPage</h1>;
 }
 // this will redirect
 // Usefull if we want to navigate user based on the component
