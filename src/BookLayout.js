2aa18ad72b5aa57ffa43333f422c613c4892112a
Animesh Raj <animesh080808@gmail.com>
2023-05-06 17:41:37 +0530

    ✨ feat : Created Book Routes to Create a scalable structure
    Signed-off-by: Animesh Raj <animesh080808@gmail.com>

--- src/BookLayout.js
+++ src/BookLayout.js
@@ -5,7 +5,7 @@ export function BookLayout() {
         <>
             <Link to="/books/1">Book 1</Link>
             <br />
-            <Link to="/books/2">Book 1</Link>
+            <Link to="/books/2">Book 2</Link>
             <br />
             <Link to="/books/new">New Book</Link>
             <br />
--- src/BookRoutes.js
+++ src/BookRoutes.js
@@ -2,13 +2,17 @@ import { Route, Routes } from 'react-router-dom';
 import { Booklist } from './pages/Booklist';
 import { Book } from './pages/Book';
 import { NewBook } from './pages/NewBook';
+import { BookLayout } from './BookLayout';
 
 export function BookRoutes() {
     return (
-        <Routes>
-            <Route index element={<Booklist />} />
-            <Route path=":id" element={<Book />} />
-            <Route path="new" element={<NewBook />} />
-        </Routes>
+        <>
+            <BookLayout />
+            <Routes>
+                <Route index element={<Booklist />} />
+                <Route path=":id" element={<Book />} />
+                <Route path="new" element={<NewBook />} />
+            </Routes>
+        </>
     );
 }
