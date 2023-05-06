948a408e5703e01f12e50e207d74107c0ba923a2
Animesh Raj <animesh080808@gmail.com>
2023-05-07 00:39:58 +0530

    ✨ feat : grab state via useLocation
    Signed-off-by: Animesh Raj <animesh080808@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
-import { Link, NavLink, Route, Routes } from 'react-router-dom';
+import { NavLink, Route, Routes, useLocation } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { NotFoundPage } from './pages/NotFoundPage';
 import { BookRoutes } from './BookRoutes';
 
 function App() {
+    const location = useLocation();
+    console.log(location);
     return (
-        // Now onwards will only work in jsx part
-
         <>
             <nav>
                 <ul>
                     <li>
-                        <NavLink to="/">Home</NavLink>
+                        <NavLink to="/" state="Hi">
+                            Home
+                        </NavLink>
                     </li>
                     <li>
                         {/* If we use end then it will only show it as active when it is active and not when its childrens are active like
