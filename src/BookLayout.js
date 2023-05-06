baac9bcd428fafd68990fa215e910cf0da634d31
Animesh Raj <animesh080808@gmail.com>
2023-05-07 00:25:25 +0530

    ✨ feat : Get Params from Url for filtering
    Signed-off-by: Animesh Raj <animesh080808@gmail.com>

--- src/BookLayout.js
+++ src/BookLayout.js
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useSearchParams } from 'react-router-dom';
 
 export function BookLayout() {
-    const [number, setNumber] = useState(3);
+    const [searchParams, setSearchParams] = useSearchParams({ n: 3 });
+    const number = searchParams.get('n');
     return (
         <>
             <Link to="/books/1">Book 1</Link>
@@ -19,7 +19,7 @@ export function BookLayout() {
             <input
                 type="number"
                 value={number}
-                onChange={(e) => setNumber(e.target.value)}
+                onChange={(e) => setSearchParams({ n: e.target.value })}
             />
         </>
     );
