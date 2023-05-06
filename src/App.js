ad755fd952e9c4eef0d576cd875e3158ea3f4bb9
Animesh Raj <animesh080808@gmail.com>
2023-05-06 16:33:30 +0530

    ✨ feat : Create Custom Component for BookLayout that will be rendered on every path of /books
    Signed-off-by: Animesh Raj <animesh080808@gmail.com>

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Booklist } from './pages/Booklist';
 import { Book } from './pages/Book';
 import { NewBook } from './pages/NewBook';
 import { NotFoundPage } from './pages/NotFoundPage';
+import { BookLayout } from './BookLayout';
 
 function App() {
     return (
@@ -21,7 +22,7 @@ function App() {
             </nav>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/books">
+                <Route path="/books" element={<BookLayout />}>
                     <Route index element={<Booklist />} />
                     <Route path=":id" element={<Book />} />
                     <Route path="new" element={<NewBook />} />
